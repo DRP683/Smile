b5b881b0e85d2db781197546ed0fe325f42c6280
DRP683 <18092@wc.school.nz>
2022-03-17 14:05:01 +1300

    Create new roues for app.py

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
-
-# comment
 @app.route('/')
 def hello_world():
-    return '<h1>Hello, World!</h1>'
+    return '<h1>Hello, world</h1>'
+def render_homepage():
+    return render_template('home.html')
 
+@app.route('/menu')
+def render_menu_page():
+    return render_template('menu.html')
 
-app.run(host="0.0.0.0")
+@app.route('/contact')
+def render_contact_page():
+    return render_template('contact.html')
+
+app.run(host='0.0.0.0')
